c805883c202f05d13122a4eaaaaf80d50289f1ea
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-02-22 08:04:23 +0000

    pycodestyle corrections

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-"""
-Simple pagination sample.
+"""Simple pagination sample.
 """
 import csv
 from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) \
-        -> Tuple[int, int]:
-    """
-    Should return a tuple of size two containing
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Should return a tuple of size two containing
     a start index and an end index corresponding
     to the range of indexes to return in a list
     for those particular pagination parameters.
@@ -20,20 +17,17 @@ def index_range(page: int, page_size: int) \
 
 
 class Server:
-    """
-    Server class to paginate a database of popular baby names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """
-        Initializes a new Server instance.
+        """Initializes a new Server instance.
         """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        Cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -44,8 +38,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Retrieves a page of data.
+        """Retrieves a page of data.
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
